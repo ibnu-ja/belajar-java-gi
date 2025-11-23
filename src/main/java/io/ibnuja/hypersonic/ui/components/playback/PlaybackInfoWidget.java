a2c55cf8b5585205ef8e9fa871945018ce5737e9
ibnu-ja <ibnu.ja@outlook.com>
2025-11-23 15:48:14 +0700

    load cover art image

--- src/main/java/io/ibnuja/hypersonic/ui/components/playback/PlaybackInfoWidget.java
+++ src/main/java/io/ibnuja/hypersonic/ui/components/playback/PlaybackInfoWidget.java
@@ -1,9 +1,12 @@
 package io.ibnuja.hypersonic.ui.components.playback;
 
 import io.ibnuja.hypersonic.Hypersonic;
+import io.ibnuja.hypersonic.state.ConnectionState;
 import io.ibnuja.hypersonic.state.Playback;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
+import org.gnome.gdk.Texture;
+import org.gnome.gdkpixbuf.PixbufLoader;
 import org.gnome.gtk.Button;
 import org.gnome.gtk.Image;
 import org.gnome.gtk.Label;
@@ -11,7 +14,13 @@ import org.javagi.gobject.annotations.InstanceInit;
 import org.javagi.gtk.annotations.GtkChild;
 import org.javagi.gtk.annotations.GtkTemplate;
 
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @SuppressWarnings("java:S110")
@@ -42,12 +51,47 @@ public class PlaybackInfoWidget extends Button {
     private void onEvent(Playback.Event event) {
         switch (event) {
             case Playback.Event.TrackChanged(var song) -> {
+                String coverArtUrl = ConnectionState.INSTANCE.getApi().getCoverArtUrl(song.getCoverArt(), 60, true);
+                if (coverArtUrl != null) {
+                    log.debug("Loading cover art from {}", coverArtUrl);
+                    loadImageFromUrl(coverArtUrl);
+                }
                 currentSongInfo.setVisible(true);
                 String markup = "<b>" + escape(song.getTitle()) + "</b>\n" + escape(song.getArtist());
                 currentSongInfo.setMarkup(markup);
             }
             case Playback.Event.PlaybackStopped _ -> currentSongInfo.setLabel("No song playing");
             default -> log.warn("Unhandled event: {}. Do nothing", event);
+        }
+    }
+
+    private void loadImageFromUrl(String url) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                byte[] imageData = downloadImage(url);
+                var loader = new PixbufLoader();
+                loader.write(imageData);
+                loader.close();
+                var pixbuf = loader.getPixbuf();
+                playingImage.setFromPaintable(Texture.forPixbuf(pixbuf));
+                playingImage.setPixelSize(60);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error("Interrupted while loading cover art", e);
+            } catch (Exception e) {
+                log.error("Failed to load cover art", e);
+            }
+        });
+    }
+
+    private byte[] downloadImage(String url) throws InterruptedException, IOException {
+        try (var client = HttpClient.newHttpClient()) {
+            HttpResponse<byte[]> response;
+            var request = HttpRequest.newBuilder()
+                    .uri(java.net.URI.create(url))
+                    .build();
+            response = client.send(request, BodyHandlers.ofByteArray());
+            return response.body();
         }
     }
 
